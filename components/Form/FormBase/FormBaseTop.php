7e760a8ea3441f3ed9f515ce7de6c3ed5e75cdb0
YjangWynter <21028927+YjangWynter@users.noreply.github.com>
2021-04-21 22:59:02 -0400

    fixed typo

--- components/Form/FormBase/FormBaseTop.php
+++ components/Form/FormBase/FormBaseTop.php
@@ -1,39 +1,6 @@
-<<<<<<< HEAD:components/Form/FormBase/FormBaseTop.php
 <?php 
 # routes
-$action_page = 'dashboard.phpu';//return to error.php
-$submit_text = 'Error';
-$options_link = 'Error';
-$options_text ='Error';
-$options_remember = false;
-$formName = 'Error';
-switch($pageName){
-    case $baseURI.'login.php':
-        #$action_page = $baseURI.'private/signin.php';
-        $submit_text = 'Log In';
-        $options_link = 'private/account_reset.php';
-        $options_text ='FORGOT PASSWORD';
-        $options_remember = true;
-        $formName = 'Signup';
-        break;
-    case $baseURI.'signup.php':
-        #$action_page = $baseURI.'private/signup.php';
-        $submit_text = 'Sign Up';
-        $options_link = 'privacy.php';
-        $options_text ='Privacy Policy';
-        $formName = 'Login';
-        break;
-}
-?>
-<div class="form_base w3-section w3-card-2 w3-auto w3-round-xxlarge">
-    <form class="w3-form form_input_group w3-section w3-mobile w3-round-xlarge "
-     action="<?php echo $action_page ?>" method="POST">
-      
-    <div class="form_input w3-auto  w3-section w3-padding">
-=======
-<?php 
-# routes
-$action_page = 'dashboard.phpu';//return to error.php
+$action_page = 'dashboard.php';//return to error.php
 $submit_text = 'Error';
 $options_link = 'Error';
 $options_text ='Error';
@@ -62,4 +29,3 @@ switch($pageName){
      action="<?php echo $action_page ?>" method="POST">
       
     <div class="form_input w3-auto  w3-section w3-padding">
->>>>>>> pr/4:components/FormBaseTop.php
